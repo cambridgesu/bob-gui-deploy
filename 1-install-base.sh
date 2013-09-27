fb21f4e2c350181c2daa241d9f9e7206d39cd411
mvl22 <martin@lucas-smith.co.uk>
2013-09-27 20:49:33 +0100

    Run mysql_secure_installation non-interactively

--- 1-install-base.sh
+++ 1-install-base.sh
@@ -45,6 +45,27 @@ if [ $dbstatus -eq 0 ]; then
         mysqladmin -u root password "${mysqlRootPassword}"
 fi
 
+# Secure MySQL (other aspects); see: https://gist.github.com/Mins/4602864
+zypper -n install -l expect
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$mysqlRootPassword\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+echo "$SECURE_MYSQL"
+
 # Create a database binding for convenience
 mysql="mysql -u root -p${mysqlRootPassword} -h localhost"
 
