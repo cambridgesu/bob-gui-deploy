cambridgesu/bob-gui-deploy

commit 53f1a928fdf299f1d150b7b935270e44299b3b2a

Copy, rather than move, the listing template

3-install-bob-gui-listing.sh
@@ -38,9 +38,9 @@ if [ "${faviconObtainFromUrl}" ] ; then
 	fi
 fi
 
-# Create the listing bootstrap file
+# Create the listing bootstrap file; it is harmless to leave the template in place
 if [ ! -e "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php ] ; then
-	mv "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php.template "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php
+	cp "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php.template "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php
 fi
 
 # Add the database credentials to the BOB listing file (replace the lines matching on the left with the whole config string on the right)

commit 57b66c1bac7fad8e4694ac6f5044fb01b577f9bf

Undo regression (retaining file ownership) introduced in 53f1a928fdf299f1d150b7b935270e44299b3b2a

3-install-bob-gui-listing.sh
@@ -40,7 +40,7 @@ fi
 
 # Create the listing bootstrap file; it is harmless to leave the template in place
 if [ ! -e "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php ] ; then
-	cp "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php.template "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php
+	cp -p "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php.template "${documentRoot}"/bob-gui/listing/bobguiListingWrapper.php
 fi
 
 # Add the database credentials to the BOB listing file (replace the lines matching on the left with the whole config string on the right)