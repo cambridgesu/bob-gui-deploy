cambridgesu/bob-gui-deploy

commit ce51930a82e6461f2527253504e617d0f0aaa64c

Use Ubuntu package rather than manual compilation for ucam_webauth module

1-install-base.sh
@@ -153,15 +153,10 @@ authModuleDirective=''
 if [ "$ravenAuth" = true ] ; then
 	
 	# Compile the Ucam-webauth Apache module if required
-	if [ ! -f ${apacheModulesDirectory}/mod_ucam_webauth.so ]; then
-		apt-get -y install libssl-dev
-		cd /tmp
-		git clone https://github.com/cambridgeuniversity/mod_ucam_webauth
-		cd mod_ucam_webauth/
-		apxs2 -c -i -lcrypto mod_ucam_webauth.c
-		cd /tmp
-		rm -rf mod_ucam_webauth/
-		cd "${SCRIPTDIRECTORY}"
+	if [ ! -f /usr/lib/apache2/modules/mod_ucam_webauth.so ]; then
+		wget -P /tmp/ https://github.com/cambridgeuniversity/mod_ucam_webauth/releases/download/v2.0.5/libapache2-mod-ucam-webauth_2.0.5apache24.ubuntu-16.04_amd64.deb
+		dpkg -i /tmp/libapache2-mod-ucam-webauth*.deb
+		rm /tmp/libapache2-mod-ucam-webauth*.deb
 	fi
 	
 	# Install Raven public key if not already present

commit 5797f8d2ae26763c530fa7be0ec6fad3694d783b

ucam_webauth seems to enable the module, so does not need to be loaded in config; regression in ce51930a82e6461f2527253504e617d0f0aaa64c

1-install-base.sh
@@ -85,7 +85,6 @@ apacheVhostsConfigDirectory=/etc/apache2/sites-available
 apacheDefaultDocumentRoot=/var/www/html
 apacheLogFilesDirectory=/var/log/apache2
 apacheVhostsRoot=/var/www
-apacheModulesDirectory=/usr/lib/apache2/modules
 apacheUser=www-data
 apacheGroup=www-data
 apacheSslKeyDirectory=/etc/ssl/private
@@ -159,7 +158,7 @@ fi
 authModuleDirective=''
 if [ "$ravenAuth" = true ] ; then
 	
-	# Compile the Ucam-webauth Apache module if required
+	# Load and enable the Ucam-webauth Apache module if required
 	if [ ! -f /usr/lib/apache2/modules/mod_ucam_webauth.so ]; then
 		wget -P /tmp/ https://github.com/cambridgeuniversity/mod_ucam_webauth/releases/download/v2.0.5/libapache2-mod-ucam-webauth_2.0.5apache24.ubuntu-16.04_amd64.deb
 		dpkg -i /tmp/libapache2-mod-ucam-webauth*.deb
@@ -178,7 +177,6 @@ if [ "$ravenAuth" = true ] ; then
 	
 	# Define a directive to include the module in the Apache configuration
 	authModuleDirective=$'\n# Raven\n'
-	authModuleDirective+=$'LoadModule ucam_webauth_module '"${apacheModulesDirectory}/mod_ucam_webauth.so"$'\n'
 	authModuleDirective+='AAKeyDir '"${apacheConfDirectory}/webauth_keys/"$'\n'
 	authModuleDirective+='AACookieKey "'"${cookieKey}"$'"\n'
 	authModuleDirective+='AAClockSkew 30'